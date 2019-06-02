9b2ad4f54d2ada6246ae41275687420f1fa3ee1f
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-02 16:35:54 +0900

    homeページにまつわるものを再度調整

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-      flash[:success] = "Rootage!"
+    flash.now[:success] = "success!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
   root 'static_pages#home'
   get 'spends/show'
   get 'incomes/show'
