6e05652bdd897ec0e0f191e5715bf7b7dd742455
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-04 12:36:44 +0900

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
+//= require jquery
 //= require bootstrap
 //= require Chart.min
-//= require jquery
-//= require rails-ujs
 //= require_tree .
