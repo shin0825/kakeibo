fbd5acba83bc92478e8f4239070b3b48c6f2bbe2
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-07 22:53:55 +0900

    数値チェックでマイナス値を許可しないよう修正

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,5 +1,5 @@
 class Income < ApplicationRecord
-  validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true }
+  validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   belongs_to :wallet
   belongs_to :income_reason
   belongs_to :user
--- app/models/spend.rb
+++ app/models/spend.rb
@@ -1,5 +1,5 @@
 class Spend < ApplicationRecord
-  validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true }
+  validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   belongs_to :wallet
   belongs_to :spend_reason
   belongs_to :user
