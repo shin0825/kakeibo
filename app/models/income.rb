0235e274ab6bf8ad93195c50d7a75d35981fd374
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-07 22:46:12 +0900

    数値入力チェックを厳密化

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,5 +1,4 @@
 class Income < ApplicationRecord
-  VALID_AMOUNT_REGEX = /\A[0-9]+\z/
   validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true }
   belongs_to :wallet
   belongs_to :income_reason
--- app/models/spend.rb
+++ app/models/spend.rb
@@ -1,6 +1,5 @@
 class Spend < ApplicationRecord
-  VALID_AMOUNT_REGEX = /\A[0-9]+\z/
-  validates :amount, presence:true, length: {maximum: 9}, format: {with: VALID_AMOUNT_REGEX }
+  validates :amount, presence:true, length: {maximum: 9}, numericality: { only_integer: true }
   belongs_to :wallet
   belongs_to :spend_reason
   belongs_to :user
