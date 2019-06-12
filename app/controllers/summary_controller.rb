7b28abfa3a009756bd3565697aad62c55232fd71
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-12 23:48:44 +0900

    仮修正

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -7,7 +7,7 @@ class SummaryController < ApplicationController
     @spend_summarys = get_spend_summary_by_reason(@p_targetDate)
     @income_summarys = get_income_summary_by_reason(@p_targetDate)
 
-    spends_chart = @spend_summarys.to_a.pluck(:reason_name, :amount)
+    spends_chart = @spend_summarys.to_a.pluck(:name, :amount)
     @spend_labels = spends_chart.map(&:first)
     @spend_datas = spends_chart.map(&:second)
 
@@ -22,21 +22,34 @@ class SummaryController < ApplicationController
   private
   def get_spend_summary_by_reason(targetDate)
 
-    summary = SpendReason.search_without_transfar()
-      .left_outer_joins(:spend)
-      .left_outer_joins(:spend_budget)
-      .select(
-        'spend_reasons.id AS reason_id',
-        'spend_reasons.name AS reason_name',
-        'SUM(spends.amount) AS amount',
-        'DATE(MAX(spends.created_at)) AS created_at',
-        'SUM(spend_budgets.amount) AS b_amount',
-        'DATE(MAX(spend_budgets.target_date)) AS target_date'
-      )
-      .where(spend_budgets: { target_date: targetDate.in_time_zone.all_month } )
-      .where(spends: { created_at: targetDate.in_time_zone.all_month } )
-      .group('spend_reasons.id', 'spend_reasons.name')
-      .order('spend_reasons.id')
+    summary = SpendReason
+      .search_without_transfar()
+      .joins('LEFT OUTER JOIN (
+          SELECT
+            spend_reason_id AS id,
+            DATE(MAX(created_at)) AS date_at,
+            SUM(amount) AS amount
+          FROM
+            spends
+          GROUP BY
+            spend_reason_id
+        ) AS spends
+        ON spends.id = spend_reasons.id')
+      .joins('LEFT OUTER JOIN (
+          SELECT
+            spend_reason_id AS id,
+            DATE(MAX(target_date)) AS date_at,
+            SUM(amount) AS amount
+          FROM
+            spend_budgets
+          GROUP BY
+            spend_reason_id
+        ) AS budgets
+        ON budgets.id = spend_reasons.id')
+        .select(:id, :name)
+        .select('COALESCE(spends.amount, 0) AS amount')
+        .select('COALESCE(budgets.amount, 0) - COALESCE(spends.amount, 0) AS by_budget_amount')
+        .order(:id)
 
     return summary
   end
