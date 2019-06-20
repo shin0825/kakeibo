2ccddd90f245e742cebf72aa51a30582f877387d
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-20 23:48:20 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,12 @@ User.create!(name:  "HO&GE",
 IncomeReason.create!(name:  "給与")
 IncomeReason.create!(name:  "賞与")
 IncomeReason.create!(name:  "もらった")
+IncomeReason.create!(id:900, name:  "調整")
 IncomeReason.create!(id:999, name:  "振替")
 SpendReason.create!(name:  "食費")
 SpendReason.create!(name:  "家賃")
 SpendReason.create!(name:  "交遊費")
+SpendReason.create!(id:900, name:  "調整")
 SpendReason.create!(id:999, name:  "振替")
 Income.create!(
   amount:  356789,
@@ -30,11 +32,11 @@ Income.create!(
   created_at: Time.new(2019, 7, 01, 9, 00, 00)
 )
 Income.create!(
-  amount:  400000,
+  amount:  4444,
   wallet_id: 3,
   income_reason_id: 2,
   user_id: 2,
-  created_at: Time.new(2019, 6, 30, 15, 00, 00)
+  created_at: Time.new(2019, 6, 30, 14, 30, 00)
 )
 Income.create!(
   amount:  5000,
