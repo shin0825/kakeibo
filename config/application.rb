ba8e3a60559a3eafa97fb627eb789884ba69d131
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-06-20 12:26:17 +0900

    time zone localize

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module Kakeibo
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
