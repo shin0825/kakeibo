1e83ccdd4a700c54d2a25bcb1d3127ec934c557b
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-07-20 17:14:34 +0900

    既存テストコードを修正し、rails tがGREENになるよう修正

--- test/controllers/spend_budgets_controller_test.rb
+++ test/controllers/spend_budgets_controller_test.rb
@@ -3,44 +3,41 @@ require 'test_helper'
 class SpendBudgetsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @spend_budget = spend_budgets(:one)
+    @user = users(:one)
+    @spend_reason = spend_reasons(:one)
   end
 
   test "should get index" do
-    get spend_budgets_url
+    get spend_budgets_index_path(:year => 2019, :month => 07)
     assert_response :success
   end
 
   test "should get new" do
-    get new_spend_budget_url
+    get new_spend_budget_path
     assert_response :success
   end
 
   test "should create spend_budget" do
     assert_difference('SpendBudget.count') do
-      post spend_budgets_url, params: { spend_budget: {  } }
+      post spend_budgets_path, params: { spend_budget: { target_date: Date.new(2019, 07, 10), amount: 3000, spend_reason_id: @spend_reason.id, user_id: @user.id, memo: ' ' } }
     end
 
-    assert_redirected_to spend_budget_url(SpendBudget.last)
-  end
-
-  test "should show spend_budget" do
-    get spend_budget_url(@spend_budget)
-    assert_response :success
+    assert_redirected_to spend_budgets_index_url(:year => (@spend_budget[:target_date]).year, :month => (@spend_budget[:target_date]).month)
   end
 
   test "should get edit" do
-    get edit_spend_budget_url(@spend_budget)
+    get edit_spend_budget_path(@spend_budget)
     assert_response :success
   end
 
   test "should update spend_budget" do
-    patch spend_budget_url(@spend_budget), params: { spend_budget: {  } }
-    assert_redirected_to spend_budget_url(@spend_budget)
+    patch spend_budget_path(@spend_budget), params: { spend_budget: { target_date: Date.new(2019, 07, 10), amount: 3500, spend_reason_id: @spend_reason.id, user_id: @user.id, memo: '' } }
+    assert_redirected_to spend_budgets_index_url(:year => (@spend_budget[:target_date]).year, :month => (@spend_budget[:target_date]).month)
   end
 
   test "should destroy spend_budget" do
     assert_difference('SpendBudget.count', -1) do
-      delete spend_budget_url(@spend_budget)
+      delete spend_budget_path(@spend_budget)
     end
 
     assert_redirected_to spend_budgets_url
